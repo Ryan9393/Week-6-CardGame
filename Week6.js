2e2a066a4c452edb2a31903458aab898b99f9e68
Ryan9393 <mryan9393@gmail.com>
2022-07-07 21:37:11 -0400

    Created Deck

--- Week6.js
+++ Week6.js
@@ -3,3 +3,32 @@
 // -	The Player who played the higher card is awarded a point
 // o	Ties result in zero points for either Player
 // -	After all cards have been played, display the score.
+
+
+class Card {
+    constructor(suit, rank, value){
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+}
+
+class Deck {
+    constructor(){
+        this.cards = [];
+    }
+    createDeck() {
+        let suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+        let ranks = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < ranks.length; j++) {
+                this.cards.push(new Card(suits[i], ranks[j], values[j]));
+            }
+        }
+    }
+}
+
+const newDeck = new Deck();
+newDeck.createDeck();
+console.log(newDeck.cards);
